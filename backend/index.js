aac55ca7a6f11d95722cf772d1d5733d6deeec54
alvaro <alvarocondeorlando@gmail.com>
2022-06-28 17:30:48 -0300

    configurando knex e iniciando migrations

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const app = require('express')();
 require('dotenv').config(); 
 const PORT = process.env.PORT;
+const db = require('./config/db');
+
+app.db = db;
+
 
 const consign = require('consign');
 
 consign()
 .then('./config/middlewares.js')
+.then('./api')
+.then('./config/routes.js')
 .into(app)
 
 app.listen(PORT, () => {
