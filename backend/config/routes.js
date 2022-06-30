5a50d392ae83af115cde4d6fee2e5860d4360a46
alvaro <alvarocondeorlando@gmail.com>
2022-06-30 17:44:13 -0300

    api de categorias

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,12 +1,24 @@
 module.exports = app => {
     app.route('/users')
-    .post(app.api.user.save)
-    .get(app.api.user.get)
+        .post(app.api.user.save)
+        .get(app.api.user.get)
 
     app.route('/users')
-    .put(app.api.user.save)
+     .put(app.api.user.save)
 
     app.route('/users/:id')
-    .get(app.api.user.getById)
+     .get(app.api.user.getById)
+
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+        
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
 }
 
